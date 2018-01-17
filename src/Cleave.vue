c9eeb0e60c8d93b42ec666e2757ee82e419950a5
Almir Davletov <davletovalmir@gmail.com>
2018-01-17 14:45:58 +0300

    Add emit focus

--- src/Cleave.vue
+++ src/Cleave.vue
@@ -3,69 +3,73 @@
 </template>
 
 <script>
-import Cleave from 'cleave.js'
-import 'cleave.js/dist/addons/cleave-phone.ru'
+  import Cleave from 'cleave.js'
+  import 'cleave.js/dist/addons/cleave-phone.ru'
 
-export default {
-  props: {
-    value: {
-      type: String,
-      default: ''
-    },
-    options: {
-      type: Object,
-      default: () => ({})
-    },
-    events: {
-      type: Object,
-      default: () => ({})
-    }
-  },
-
-  data () {
-    return {
-      cleave: null
-    }
-  },
-
-  methods: {
-    emitEvent () {
-      this.$emit('input', this.$el.value)
-      this.$emit('rawValueChanged', this.cleave.getRawValue())
-    }
-  },
-
-  mounted () {
-    this.$el.value = this.value
-    this.cleave = new Cleave(this.$el, this.options)
-    Object.keys(this.events).map((key) => {
-      this.$refs.input.addEventListener(key, this.events[key])
-    })
-    if (this.options.maxLength) {
-      this.$el.setAttribute('maxlength', this.options.maxLength)
-    }
-
-    // in case of cleave.js remove result or properties from cleave instance.
-    if (this.cleave.properties && this.cleave.properties.hasOwnProperty('result')) {
-      this.$watch('cleave.properties.result', this.emitEvent)
-    } else {
-      this.$el.addEventListener('input', this.emitEvent)
-    }
-  },
-
-  beforeDestroy () {
-    this.cleave.destroy()
-  },
-
-  watch: {
-    options: {
-      deep: true,
-      handler (val) {
-        this.cleave.destroy()
-        this.cleave = new Cleave(this.$el, val)
+  export default {
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      options: {
+        type: Object,
+        default: () => ({})
+      },
+      events: {
+        type: Object,
+        default: () => ({})
       }
-    }
-  },
+    },
 
-}
+    data () {
+      return {
+        cleave: null
+      }
+    },
+
+    methods: {
+      emitInput () {
+        this.$emit('input', this.$el.value)
+        this.$emit('rawValueChanged', this.cleave.getRawValue())
+      },
+      emitFocus () {
+        this.$emit('focus', this.$el)
+      }
+    },
+
+    mounted () {
+      this.$el.value = this.value
+      this.cleave = new Cleave(this.$el, this.options)
+      Object.keys(this.events).map((key) => {
+        this.$refs.input.addEventListener(key, this.events[key])
+      })
+      if (this.options.maxLength) {
+        this.$el.setAttribute('maxlength', this.options.maxLength)
+      }
+
+      // in case of cleave.js remove result or properties from cleave instance.
+      if (this.cleave.properties && this.cleave.properties.hasOwnProperty('result')) {
+        this.$watch('cleave.properties.result', this.emitInput)
+      } else {
+        this.$el.addEventListener('input', this.emitInput)
+      }
+
+      this.$el.addEventListener('focus', this.emitFocus)
+    },
+
+    beforeDestroy () {
+      this.cleave.destroy()
+    },
+
+    watch: {
+      options: {
+        deep: true,
+        handler (val) {
+          this.cleave.destroy()
+          this.cleave = new Cleave(this.$el, val)
+        }
+      }
+    },
+  }
 </script>
